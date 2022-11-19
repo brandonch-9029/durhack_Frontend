103c48151434b3c31f2acc3c735c29c69796be8a
Samuel Yiu <samuelyiusy@gmail.com>
2022-11-19 20:51:41 +0000

    reverted change to match live version

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function showPreview(event){
-    if(event.target.files.length > 0){
-      var src = URL.createObjectURL(event.target.files[0]);
-      var imgElement = document.getElementById("imgfile");
-      imgElement.src = src;
-      imgElement.style.display = "block";
-    }
+  if(event.target.files.length > 0){
+    var src = URL.createObjectURL(event.target.files[0]);
+    var imgElement = document.getElementById("file-ip-1-preview");
+    imgElement.src = src;
+    imgElement.style.display = "block";
   }
+}
